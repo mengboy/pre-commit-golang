95ee638b0808d549f9f68ee59d078b62462f15fa
mengboy <bai.white86@gmail.com>
2019-09-15 19:17:43 +0800

    first commit

---
+++ go-fmt.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+STAGED_GO_FILES=$(git diff --cached --name-only --diff-filter=ACM | grep ".go$")
+
+if [[ "$STAGED_GO_FILES" = "" ]]; then
+    exit 0
+fi
+
+PASS=true
+
+for FILE in $STAGED_GO_FILES
+do
+    # ignore verdor/
+    if [[ $FILE == "vendor"* ]];then
+        continue
+    fi
+
+    # check go import 
+    goimports -w $FILE
+    if [[ $? != 0 ]]; then
+        PASS=false
+    fi
+
+
+  # if not format, exec gofmt 
+    UNFORMATTED=$(gofmt -l $FILE)
+    if [[ "$UNFORMATTED" != "" ]];then
+        gofmt -w $PWD/$UNFORMATTED
+        if [[ $? != 0 ]]; then
+            PASS=false
+        fi
+    fi
+
+    # add goimports gofmt change
+    git add $FILE
+
+done
+
+if ! $PASS; then
+    printf "\033[31m COMMIT FAILED \033[0m\n"
+    exit 1
+else
+    printf "\033[32m COMMIT SUCCEEDED \033[0m\n"
+fi
+
+exit 0
---
+++ go-vet.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+ 
+# git go vet pre-commit hook
+
+if [ -z "$GOPATH" ]; then
+	echo "ERROR: pre-commit hook for go vet: \$GOPATH is empty. Please run 'source dev.env' to set the correct \$GOPATH."
+	exit 1
+fi
+
+# This script does not handle file names that contain spaces.
+gofiles=$(git diff --cached --name-only --diff-filter=ACM | grep '\.go$')
+
+
+if [[ "$STAGED_GO_FILES" = "" ]]; then
+    exit 0
+fi
+
+# If any checks are found to be useless, they can be disabled here.
+# See the output of "go tool vet" for a list of flags.
+vetflags="-all=true"
+
+errors=
+
+# Run on one file at a time because a single invocation of "go tool vet"
+# with multiple files requires the files to all be in one package.
+for gofile in $gofiles
+do
+
+    # ignore vendor/
+    if [[ $FILE == "vendor"* ]];then
+        continue
+    fi
+
+	if ! go tool vet $vetflags $gofile 2>&1; then
+		errors=YES
+	fi
+done
+
+[ -z  "$errors" ] && exit 0
+
+echo
+echo "Please fix the go vet warnings above. To disable certain checks, change vetflags in misc/git/hooks/govet."
+exit 1
